83a9d11207885244e4fed7c50a978e9dbe1bd7c5
DanyKrk <daniel.k.8576@gmail.com>
2022-05-25 20:57:54 +0200

    finished, still needs refactoring

---
+++ cw09/zad1/elf.h
@@ -0,0 +1,41 @@
+#ifndef OPERATING_SYSTEMS_ELF_H
+#define OPERATING_SYSTEMS_ELF_H
+
+#include "header.h"
+
+void elf_wait(int ID) {
+    pthread_mutex_lock(&mutex_elf_wait);
+    while (waiting_elves == MAX_WAITING_ELVES) {
+        printf("Elf: czeka na powrót elfów, ID: %d\n", ID);
+        pthread_cond_wait(&cond_elf_wait, &mutex_elf_wait);
+    }
+    pthread_mutex_unlock(&mutex_elf_wait);
+}
+
+void elf_issue(int ID) {
+    pthread_mutex_lock(&mutex_elf);
+    if (waiting_elves < MAX_WAITING_ELVES) {
+        elves_queue[waiting_elves] = ID;
+        waiting_elves++;
+        printf("Elf: czeka [%d] elfów na Mikołaja, ID: %d\n", waiting_elves, ID);
+
+        if (waiting_elves == MAX_WAITING_ELVES){
+            printf("Elf: wybudzam Mikołaja, ID: %d\n", ID);
+            pthread_mutex_lock(&mutex_santa);
+            pthread_cond_broadcast(&cond_santa);
+            pthread_mutex_unlock(&mutex_santa);
+        }
+    }
+    pthread_mutex_unlock(&mutex_elf);
+}
+
+void* elf(void* arg){
+    int ID = *((int *) arg);
+    while(true){
+        sleep(ELF_WORKING_TIME);
+        elf_wait(ID);
+        elf_issue(ID);
+    }
+}
+
+#endif //OPERATING_SYSTEMS_ELF_H
---
+++ cw09/zad1/reindeer.h
@@ -0,0 +1,8 @@
+//
+// Created by daniel on 25.05.22.
+//
+
+#ifndef OPERATING_SYSTEMS_RAINDEER_H
+#define OPERATING_SYSTEMS_RAINDEER_H
+
+#endif //OPERATING_SYSTEMS_RAINDEER_H
